7365d0f8ab447f79c01f6ebca47ded0d2ddb360f
JJIMINSHIN <scalla1031@gmail.com>
2022-08-04 13:19:48 +0900

    Main 페이지 ui 수정

--- client/src/Main.js
+++ client/src/Main.js
@@ -10,9 +10,15 @@ const Main = () => {
 
     return (
         <div className="ai">
-           <div>
-            메인
-           </div>
+            <div className="left" style={{ width: '50%', height: '100%', float: 'left', backgroundColor: 'violet', color: 'white' }}>
+                이부분 css빼서 left, right로 나눠주세용
+                여기가 left
+            </div>
+
+            <div className="right" style={{ width: '50%', height: '100%', float: 'right', backgroundColor: 'lightskyblue', color: 'white' }}>
+                여기가 right
+            </div>
+
         </div>
 
     ) //return
