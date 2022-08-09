d09f226af6521357485f925f7518af1d1ad91955
JJIMINSHIN <scalla1031@gmail.com>
2022-08-09 21:36:41 +0900

    readme update

--- server/config/jwtConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    "secret" : "pAqsUFEzfsI1Zr9ZNSf4_x1xhx8fYVidspABaQQkGzY"
-}
-
--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = require('./schemas/User');
-
-exports.User = mongoose.model("User", UserSchema)
