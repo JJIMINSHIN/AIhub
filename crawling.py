17dab02af1a1f4b2fd45431d2aaa403f0e70d70f
JJIMINSHIN <scalla1031@gmail.com>
2022-08-10 14:32:46 +0900

    토끼상

--- crawling.py
+++ crawling.py
@@ -4,6 +4,6 @@ response = google_images_download.googleimagesdownload()   #class instantiation
 
 arguments = { 
     "keywords":
-    "박성웅","limit":30,"print_urls":True, "format": "jpg"}   #creating list of arguments
+    "임윤아, 걸스데이 유라, 민효린","limit":30,"print_urls":True, "format": "jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
