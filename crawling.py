00cad3548ca1c10b555829d15586e8c8f730f55a
JJIMINSHIN <scalla1031@gmail.com>
2022-08-11 22:58:02 +0900

    fix

--- crawling.py
+++ crawling.py
@@ -4,6 +4,6 @@ response = google_images_download.googleimagesdownload()   #class instantiation
 
 arguments = { 
     "keywords":
-    "우영우 정명석, 엑소 백현, 박보검, 송중기, 황민현, 엑소 시우민, 강동원, 이종석, 이준기, 마동석, 조진웅, 박성웅, 손석구, 윤두준, 이민기, 김우빈,  엑소 수호","limit":5,"print_urls":True, "format": "jpg"}   #creating list of arguments
+    "이석훈, 셔누, 유승호","limit":30,"print_urls":True, "format": "jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
