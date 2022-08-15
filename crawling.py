07b08c19d67438d70f38eb1f293b5010725e3e0e
Sososoy <87933620+Sososoy@users.noreply.github.com>
2022-08-15 19:12:36 +0900

    결과 랜덤  이미지 완성

--- crawling.py
+++ crawling.py
@@ -4,7 +4,7 @@ response = google_images_download.googleimagesdownload()   #class instantiation
 
 arguments = { 
     "keywords":
-    "이석훈, 셔누, 유승호","limit":30,"print_urls":True, "format": "jpg"}   #creating list of arguments
+    "수지, 나연, 아이린, 공승연","limit":5,"print_urls":True, "format": "jpg"}   #creating list of arguments
 
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
