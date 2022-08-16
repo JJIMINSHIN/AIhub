646158de76eb58a61a958206daa09cf7df2d64c3
Sososoy <87933620+Sososoy@users.noreply.github.com>
2022-08-16 13:09:51 +0900

    index 수정

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import '../App.css';
 import { Button } from './Button';
+import { useState } from "react";
 import './Main.css';
+import './../'
+
 
 function HeroSection() {
+
+  // view를 변경하기 위한 useState
+  const [view, setView] = useState({
+    signIn: false,
+    signUp: false
+});
+const onButtonEvent = () =>{
+  window.location.href='main.html'
+}
   return (
+    <main>
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>당신의 미래 애인상을<br />찾아드립니다.</h1>
       <p>내 얼굴형으로 추측하는 <br />나의 미래 애인상은 과연 누가 될까요?</p>
       <div className='hero-btns'>
-        <Button
+          <Button onClick={onButtonEvent}
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
@@ -19,6 +32,10 @@ function HeroSection() {
         </Button>
       </div>
     </div>
+  {
+    view.signIn ? (<s/>) : (<></>)
+  }
+  </main>
   );
 }
 
