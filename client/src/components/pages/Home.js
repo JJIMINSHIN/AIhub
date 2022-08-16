a8732c5f5261448806638a8f744e82d37f00d56a
JJIMINSHIN <scalla1031@gmail.com>
2022-08-16 20:28:24 +0900

    result img 오류 수정 완료

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../../App.css';
 import Main from '../Main';
-import Footer from '../Footer';
 
 function Home() {
   return (
     <>
         <Main />
-        <Footer />
     </>
   );
 }
