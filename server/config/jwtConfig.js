cf81ee2c7a6589b0ce8a3e1634beb55e0bcc217e
JJIMINSHIN <scalla1031@gmail.com>
2022-08-23 19:00:52 +0900

    server active change

--- server/config/jwtConfig.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    "secret" : "f32smCq2CgcUg2-_a3TxKu04iJcIqKAmR1TbM9v6Mqg"
-}
--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = require('./Schemas/User');
-
-exports.User = mongoose.model('User', UserSchema)
